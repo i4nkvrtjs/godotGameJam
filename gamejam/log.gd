f1f69bdd605a57cb5d5ccfb836c64c3395f884f1
i4nkvrtjs <valentin.pineyro@gmail.com>
2025-04-10 16:26:12 -0300

    mas cambios al log

--- gamejam/log.gd
+++ gamejam/log.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@export var game_manager: Node # Reference to GameManager
+@export var game_manager: Node
 @export var category_options: Dictionary = {
 	"Habitat": ["Bosque", "Pradera", "Monte", "Lago", "Cueva"],
 	"Actividad": ["Día", "Noche"],
@@ -13,8 +13,10 @@ var button_paths = {
 }
 
 func _ready():
-	process_mode = Node.PROCESS_MODE_ALWAYS # Allows interaction even when game is paused
+	process_mode = Node.PROCESS_MODE_ALWAYS 
 	update_ui()
+	hide_log()
+	set_process_unhandled_input(true)
 
 func update_ui():
 	for category in category_options.keys():
@@ -23,7 +25,6 @@ func update_ui():
 			button.clear()
 			for option in category_options[category]:
 				button.add_item(option)
-		#button.selected = 0  # Default to the first option
 
 func _on_option_selected(index: int, button: OptionButton, category: String):
 	print(category, "selected:", button.get_item_text(index))
@@ -38,23 +39,13 @@ func show_log():
 	visible = true
 	get_tree().paused = true
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	set_process_input(true)
+	set_process_unhandled_input(true)
 
 func hide_log():
 	visible = false
 	get_tree().paused = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	set_process_input(false)
-	
-
-func _input(event: InputEvent):
-	if event.is_action_pressed("toggleLog"):
-		toggle_log()
-
-
-#func _on_option_button_item_selected(index: int, button: OptionButton, category: String):
-#	print(category, "selected:", button.get_item_text(index))
-
+	set_process_unhandled_input(true)
 
 func _on_cerrar_pressed():
 	hide_log()
